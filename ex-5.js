e950d1449e47361a3deb26dccedd826cd256e8aa
Palm <palml1lax@gmail.com>
2023-11-23 12:06:20 +0700

    feat: finish exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discountPercentage = 25;
+
+totalPrice = totalPrice - (totalPrice / 100) * 25;
+
+console.log(totalPrice);
--- ex-7.js
+++ ex-7.js
@@ -5,3 +5,7 @@ let verb = "am";
 let object = "A Software Developer";
 
 let sentence;
+
+sentence = subject + " " + verb + " " + object + ".";
+
+console.log(sentence);
